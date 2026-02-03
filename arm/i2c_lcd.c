ac8d4078c358e99662cf4c4421847bf668703255
KSK3939 <rlatjsrb777@gmail.com>
2026-02-03 14:21:16 +0900

    Update i2c_lcd.c

--- arm/i2c_lcd.c
+++ arm/i2c_lcd.c
@@ -8,10 +8,11 @@ void lcd_command(uint8_t command)
   uint8_t i2c_buffer[4];
   high_nibble = command & 0xf0;
   low_nibble = (command << 4) & 0xf0;
-  i2c_buffer[0] = high_nibble | 0x04 | 0x08; // en = 1, rs = 0, rw = 0, backligh = 1
-  i2c_buffer[1] = high_nibble | 0x00 | 0x08; // en = 0, rs = 0, rw = 0, backligh = 1
-  i2c_buffer[2] = low_nibble  | 0x04 | 0x08; // en = 1, rs = 0, rw = 0, backligh = 1
-  i2c_buffer[3] = low_nibble  | 0x00 | 0x08; // en = 0, rs = 0, rw = 0, backligh = 1
+  // en, rs, rw, backlight는 하위 비트로 컨트롤
+  i2c_buffer[0] = high_nibble | 0x04 | 0x08; // en = 1, rs = 0, rw = 0, backlight = 1
+  i2c_buffer[1] = high_nibble | 0x00 | 0x08; // en = 0, rs = 0, rw = 0, backlight = 1
+  i2c_buffer[2] = low_nibble  | 0x04 | 0x08; // en = 1, rs = 0, rw = 0, backlight = 1
+  i2c_buffer[3] = low_nibble  | 0x00 | 0x08; // en = 0, rs = 0, rw = 0, backlight = 1
   while(HAL_I2C_Master_Transmit(&hi2c1, I2C_LCD_ADDRESS, i2c_buffer, 4, 100) != HAL_OK);
 }
 
@@ -21,10 +22,10 @@ void lcd_data(uint8_t data)
   uint8_t i2c_buffer[4];
   high_nibble = data & 0xf0;
   low_nibble = (data << 4) & 0xf0;
-  i2c_buffer[0] = high_nibble | 0x05 | 0x08; // en = 1, rs = 1, rw = 0, backligh = 1
-  i2c_buffer[1] = high_nibble | 0x01 | 0x08; // en = 0, rs = 1, rw = 0, backligh = 1
-  i2c_buffer[2] = low_nibble  | 0x05 | 0x08; // en = 1, rs = 1, rw = 0, backligh = 1
-  i2c_buffer[3] = low_nibble  | 0x01 | 0x08; // en = 0, rs = 1, rw = 0, backligh = 1
+  i2c_buffer[0] = high_nibble | 0x05 | 0x08; // en = 1, rs = 1, rw = 0, backlight = 1
+  i2c_buffer[1] = high_nibble | 0x01 | 0x08; // en = 0, rs = 1, rw = 0, backlight = 1
+  i2c_buffer[2] = low_nibble  | 0x05 | 0x08; // en = 1, rs = 1, rw = 0, backlight = 1
+  i2c_buffer[3] = low_nibble  | 0x01 | 0x08; // en = 0, rs = 1, rw = 0, backlight = 1
   while(HAL_I2C_Master_Transmit(&hi2c1, I2C_LCD_ADDRESS, i2c_buffer, 4, 100) != HAL_OK);
 }
 
